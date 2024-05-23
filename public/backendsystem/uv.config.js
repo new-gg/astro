d636fdd4c017c12a1436bd6ec797a3b2b206f992
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-05-23 01:00:11 +0000

    attempt

--- public/backendsystem/uv.config.js
+++ public/backendsystem/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: "/astro~/",
+    prefix: "/service/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/backendsystem/uv.handler.js",
