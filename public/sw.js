1163152cbf1e67b6c2365469055124cb3f626d71
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-05-23 01:04:50 +0000

    sw fixed

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,17 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
-importScripts('/uv/uv.sw.js');
+importScripts('../epoxy/index.js');
+importScripts('uv.bundle.js');
+importScripts('uv.config.js');
+importScripts(__uv$config.sw || 'uv.sw.js');
 
-const sw = new UVServiceWorker();
+const uv = new UVServiceWorker();
 
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        (async ()=>{
+            if(uv.route(event)) {
+                return await uv.fetch(event);
+            }
+            return await fetch(event.request);
+        })()
+    );
+});
